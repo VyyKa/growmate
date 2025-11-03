1dacb4fd174bd9410b5843b6b38d90e5513908cf
VyyKa <cybersecvykha@gmail.com>
2025-11-03 19:13:20 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/logo.png"
+import Logo from "../assets/Logo.png"
 import LoginButton from "./LoginButton"
 import AvatarDropdown from "./AvatarDropdown"
 import ContactButton from "./ContactButton"
